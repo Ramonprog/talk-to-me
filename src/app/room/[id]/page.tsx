c896a7588e97735e517eb87a12ec7857d7e3c118
Ramonprog <programador.ramon@gmail.com>
2024-02-16 20:59:23 -0300

    aceitando novos usuarios

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -8,17 +8,43 @@ import { useContext, useEffect, useRef } from "react";
 export default function Room({ params }: { params: { id: string } }) {
     const { socket } = useContext(SocketContext)
     const localStrem = useRef<HTMLVideoElement>(null)
+    const peerConections = useRef<Record<string, RTCPeerConnection>>({})
 
     useEffect(() => {
         socket?.on('connect', async () => {
-            console.log('connected')
             socket?.emit('subscribe', {
                 roomId: params.id,
                 socketId: socket.id,
             });
             await initCamera()
         })
+
+        socket?.on('new user', (data) => {
+            createPeerConnection(data.socketId)
+            socket.emit('newUserStart', {
+                to: data.socketId,
+                sender: socket.id
+            })
+        })
+
+        socket?.on('newUserStart', (data) => {
+            console.log('usuário conectado na sala', data)
+        })
     }, [socket, params.id])
+
+    const createPeerConnection = (socketId: string) => {
+        const config = {
+            iceServers: [
+                {
+                    urls: 'stun:stun.l.google.com:19302'
+                }
+            ]
+        }
+
+        const peer = new RTCPeerConnection(config)
+        //na posição desse id ele recebe a peer que é a conexão rtc
+        peerConections.current[socketId] = peer
+    }
 
     const initCamera = async () => {
         const video = await navigator.mediaDevices.getUserMedia({
@@ -40,7 +66,7 @@ export default function Room({ params }: { params: { id: string } }) {
                 <div className="md:w-[85%] w-full m-3 ">
                     <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
                         <div className="bg-gray-950 h-full rounded-md w-full p-2  relative">
-                            <video autoPlay playsInline ref={localStrem}></video>
+                            <video autoPlay playsInline ref={localStrem} className="h-full w-full mirror-mode"></video>
                             <span className="absolute bottom-2">Alisson Ramon</span>
                         </div>
                         <div className="bg-gray-950 h-full rounded-md p-2  w-full relative">
